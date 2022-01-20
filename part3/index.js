80651422e75898a9054e520795b6ec8e33df1cd5
Neil Ruaro <erickson_ruaroii@dlsu.edu.ph>
2022-01-20 09:19:12 +0800

    finished part 3.1

--- part3/index.js
+++ part3/index.js
@@ -56,6 +56,35 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end();
 });
 
+const generateId = () => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+
+    return maxId + 1;
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body;
+
+    console.log(body);
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content is missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
+});
+
 app.get('/github', (request, response) => {
     const githubAccount = "https://github.com/eRuaro";
     response.redirect(githubAccount);
