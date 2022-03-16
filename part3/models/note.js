1ca347d89da17d8f5341adf25091987b9a88377a
Neil Ruaro <erickson_ruaroii@dlsu.edu.ph>
2022-03-16 19:48:06 +0800

    started part4

--- part3/models/note.js
+++ part3/models/note.js
@@ -1,20 +1,15 @@
 const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URI;
-
-console.log("connecting to", url);
-mongoose.connect(url)
-  .then(result => {
-    console.log(result);
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
-
 const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
+  content: {
+    type: String,
+    minlength: 5,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
   important: Boolean,
 });
 
