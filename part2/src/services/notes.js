fa6b519b70756ec9ca2547a9b4365843e37a0c9a
Neil Ruaro <erickson_ruaroii@dlsu.edu.ph>
2022-04-19 15:48:37 +0800

    finished part 4b

--- part2/src/services/notes.js
+++ part2/src/services/notes.js
@@ -19,4 +19,8 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default { getAll, create, update }
+export default { 
+  getAll, 
+  create,
+  update 
+}
