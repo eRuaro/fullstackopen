b1f888ef0bd1c7d229ea52b685c0820439214db5
Neil Ruaro <erickson_ruaroii@dlsu.edu.ph>
2022-05-18 21:11:49 +0800

    started working on usermanagement

--- part13/models/index.js
+++ part13/models/index.js
@@ -1,7 +1,13 @@
 const Note = require("./note");
+const User = require("./user");
 
-Note.sync();
+// there is a one to many relationship between users and notes
+User.hasMany(Note);
+Note.belongsTo(User);
+Note.sync({ alter:true });
+User.sync({ alter: true });
 
 module.exports = {
-    Note
+    Note, 
+    User
 }
