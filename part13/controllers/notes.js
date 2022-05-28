675bdc218388a84ebdf205dc7a89eb39ba20e845
Neil Ruaro <erickson_ruaroii@dlsu.edu.ph>
2022-05-28 17:21:17 +0800

    completed admin and user disabling section

--- part13/controllers/notes.js
+++ part13/controllers/notes.js
@@ -8,21 +8,6 @@ const noteFinder = async (req, res, next) => {
     next();
 }
 
-const tokenExtractor = (req, res, next) => {
-    const authorization = req.get("authorization");
-    if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-        try {
-            req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
-        } catch {
-            res.status(401).json({ error: "invalid token" });
-        } 
-    } else {
-        res.status(401).json({ error: "token not found" });
-    }
-
-    next();
-}
-
 router.get("/", async (req, res) => {
     let important = {
         [Op.in]: [true, false]
